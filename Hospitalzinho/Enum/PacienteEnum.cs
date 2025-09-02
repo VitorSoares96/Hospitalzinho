4376054b266eec63291c3097805fd5577bfa6feb
Vinícius Estevão <vinicius.estevao@interfocus.com.br>
2025-09-02 15:51:14 -0300

    Criando o necessario para Paciente

---
+++ Hospitalzinho/Enum/PacienteEnum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospitalzinho.Enum
+{
+    public enum SexoPaciente
+    {
+        Masculino = 1,
+        Feminino = 2,
+        Outro = 3
+    }
+
+    public enum RacaPaciente
+    {
+        Branca = 1,
+        Preta = 2,
+        Parda = 3,
+        Amarela = 4,
+        Indigena = 5
+    }
+
+    public enum EscolaridadePaciente
+    {
+        Analfabeto = 1,
+        FundamentalIncompleto = 2,
+        FundamentalCompleto = 3,
+        MedioIncompleto = 4,
+        MedioCompleto = 5,
+        SuperiorIncompleto = 6,
+        SuperiorCompleto = 7,
+        PosGraduacao = 8,
+        NaoSeAplica = 9
+    }   
+}
