b635fdcb2147c9ac20ddb016709864cb4183c0d8
VINICIUS ESTEVAO CONSOLINO BRANDI <vviniciusbrandi@gmail.com>
2025-09-03 22:06:33 -0300

    Add files via upload

---
+++ Hospitalzinho/Hospitalzinho/Entidades/PacienteContato.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospitalzinho.Entidades
+{
+    public class PacienteContato
+    {
+        public virtual long Id { get; set; }
+        public virtual Paciente Paciente { get; set; }
+        public virtual string TelefoneResidencial { get; set; }
+        public virtual string TelefoneCelular { get; set; }
+        public virtual string Email { get; set; }
+    }
+}
---
+++ Hospitalzinho/Hospitalzinho/Entidades/PacienteProntuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Hospitalzinho.Entidades
+{
+    public class PacienteProntuario
+    {
+        public virtual long Id { get; set; }
+        public virtual Paciente Paciente { get; set; }
+        public virtual string TipoSanguineo { get; set; }
+        public virtual string Alergias { get; set; }
+        public virtual string DoencasCronicas { get; set; }
+        public virtual string MedicacoesUsoContinuo { get; set; }
+        public virtual string HistoricoCirurgico { get; set; }
+        public virtual string Vacinas { get; set; }
+        public virtual DateTime DataAbertura { get; set; }
+        public virtual DateTime UltimaAtualizacao { get; set; }
+
+        public virtual IList<PacienteConsulta> Consultas { get; set; } = new List<Consulta>();
+        public virtual IList<PacienteInternacao> Internacoes { get; set; } = new List<Internacao>();
+        public virtual IList<PacienteExame> Exames { get; set; } = new List<Exame>();
+    }
+}
