7bb83f7b93fba13c1a83c88cd80cc888a9f5f927
MAURICIO SHIGUEMITSU KAMADO IKEDA <mauricioikeda17@gmail.com>
2025-09-04 14:59:25 -0300

    Começando MVC

---
+++ HospitalzinhoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HospitalzinhoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
