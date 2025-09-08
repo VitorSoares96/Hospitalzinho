f7f63619b94137428fb9809665b413e531c9391f
Vinícius Estevão <vinicius.estevao@interfocus.com.br>
2025-09-08 15:40:23 -0300

    Implementação do repositorio
    Onde irei fazer a ligação do banco de dados com o codigo

---
+++ Hospitalzinho/Repositorio/IRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospitalzinho.Repositorio
+{
+    public interface IRepositorio
+    {
+        void Incluir(object model);
+        void Salvar(object model);
+        void Excluir(object model);
+        T ConsultarPorId<T>(long id);
+        IQueryable<T> Consultar<T>();
+
+        IDisposable IniciarTransacao();
+        void Commit();
+        void Rollback();
+    }
+
+}
