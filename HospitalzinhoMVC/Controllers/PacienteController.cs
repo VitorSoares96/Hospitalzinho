c476bbd91748fbe54f6a25e5a79e5d7b33ca8cee
MAURICIO SHIGUEMITSU KAMADO IKEDA <mauricioikeda17@gmail.com>
2025-10-01 22:24:21 -0300

    Adicionando post do buscar paciente e melhorando algumas coisas

--- HospitalzinhoMVC/Controllers/PacienteController.cs
+++ HospitalzinhoMVC/Controllers/PacienteController.cs
@@ -9,9 +9,31 @@ namespace HospitalzinhoMVC.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Consulta()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Consulta(string cpfPaciente)
+        {
+            if (String.IsNullOrWhiteSpace(cpfPaciente))
+            {
+                Console.WriteLine("CPF inválido ou vazio");
+                return View();
+            }
+
+            cpfPaciente = cpfPaciente.Replace(".", "").Replace("-", "");
+
+            if (cpfPaciente.Any(char.IsLetter))
+            {
+                Console.WriteLine("CPF não pode conter letras");
+                return View();
+            }
+
+            return View();
+        }
     }
 }
+    
